64beaaaeb796c0d753725ec17804dec2aad9ac36
tmy_sam <TSS@mail.tmysam.top>
2019-04-05 08:24:52 +0800

    with background

---
+++ apis/indexload.php
@@ -0,0 +1,21 @@
+<?php
+include "server-info.php";
+session_start();
+if(!isset($_SESSION["username"])){
+    $username="GUEST";
+}else{
+    $username=$_SESSION["username"];
+}
+$database = new mysqli($dbhost,$dbuser,$dbpawd,$dbname);
+$result = $database->query("SELECT author,time,stars,title,threadid FROM articles ORDER BY threadid DESC LIMIT 10");
+$output = array("articles"=>array(),"tags"=>array(),"user"=>array("username"=>$username),"tags"=>array());
+while($row = mysqli_fetch_array($result)){
+    $output['articles'][]=array("author"=>$row["author"],"time"=>date("Y-m-d",$row["time"]),"stars"=>$row["stars"],"title"=>$row["title"],"threadid"=>$row["threadid"]);
+}
+$result = $database->query("SELECT tags FROM tags LIMIT 10");
+while($row = mysqli_fetch_array($result)){
+    $output['tags'][]=$row["tags"];
+}
+header('Content-type: application/json');
+print(json_encode($output));
+?>
---
+++ js/ajax.js
@@ -0,0 +1,17 @@
+ajaxhttp=new XMLHttpRequest();
+articlesDiv=document.getElementById("articles");
+userDiv=document.getElementById("userinfo");
+ajaxhttp.onreadystatechange=function(){
+    if (ajaxhttp.readyState==4 && ajaxhttp.status==200){
+        data=JSON.parse(ajaxhttp.responseText);
+        userDiv.innerHTML='<h3><i class="iconfont icon-author"></i>'+data["user"]["username"]+'</h3>';
+        var output='';
+        for (var i = 0; i < data["articles"].length; i++) {  
+            preoutput='<div class="article"><h2>'+data["articles"][i]["title"]+'</h2><h4 class="leftinfo"><i class="iconfont icon-author"></i>'+data["articles"][i]["author"]+'</h4><h4 class="rightinfo"><i class="iconfont icon-starmarkhighligh"></i>'+data["articles"][i]["stars"]+'&nbsp;<i class="iconfont icon-shijian"></i>'+data["articles"][i]["time"]+'</h4><div class="lineofarticle"></div></div>';
+            output=output+preoutput;
+        }
+        articlesDiv.innerHTML=output;
+    }
+}
+ajaxhttp.open("GET","./apis/indexload.php",true);
+ajaxhttp.send();
